3cd12b56c7f33d898659c6daed8eed1c704ae231
Yara <yaramohmedd404@gmail.com>
2025-12-12 20:43:37 +0200

    final

--- src/bank/managment/system/MiniStatement.java
+++ src/bank/managment/system/MiniStatement.java
@@ -1,3 +1,4 @@
+/*
 package bank.managment.system;
 
 import java.sql.SQLException;
@@ -175,15 +176,9 @@ public class MiniStatement extends javax.swing.JFrame {
     this.dispose();
     }                                    
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -199,7 +194,7 @@ public class MiniStatement extends javax.swing.JFrame {
         
         //</editor-fold>
 
-        /* Create and display the form */
+        
       String cardNumber = "4587XXXXXXXX7896"; 
     java.awt.EventQueue.invokeLater(new Runnable() {
         public void run() {
@@ -214,3 +209,4 @@ public class MiniStatement extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     // End of variables declaration                   
 }
+*/
